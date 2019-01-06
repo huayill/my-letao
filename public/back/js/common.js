dbca6c99c920adb147ecfcbb5e0cbc18e473029b
huayill <1392772648@qq.com>
2019-01-06 10:20:06 +0800

    登录拦截完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -10,7 +10,30 @@ $(document).ajaxStop(function(){
 
 //左侧二级菜单
 $(function(){
-    $('.lt_aside .nav .category').on('click',function(){
+    $('.lt_aside .category').click(function(){
         $(this).next().stop().slideToggle();
+    })
+    //左边侧边栏切换
+    $('.icon_menu').click(function(){
+        $('.lt_aside').toggleClass('hidemenu');
+        $('.lt_main').toggleClass('hidemenu');
+        $('.topbar').toggleClass('hidemenu');
+    })
+    // 退出功能
+    $('.icon_logout').click(function(){
+        $('#logoutModal').modal("show");
+    })
+    $('.logoutBtn').click(function(){
+        $.ajax({
+            type:'get',
+            dataType:'json',
+            url:'/employee/employeeLogout',
+            success:function(info){
+                console.log(info);
+                if(info.success){
+                    location.href = 'login.html';
+                }
+            }
+        })
     })
 })
